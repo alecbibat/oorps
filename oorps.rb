058d4c4dd4ecf3745d561c466ed1373f80ee7751
Alec Bibat <alecbibat@gmail.com>
2015-06-04 19:22:43 -0600

    finish oorps

--- oorps.rb
+++ oorps.rb
@@ -1,5 +1,28 @@
+class Hand
+  include Comparable
+
+  attr_reader :value
+
+  def initialize(v)
+    @value = v
+  end
+
+  def <=>(another_hand)
+    if @value == another_hand.value
+      0
+    elsif (@value == 'p' && another_hand.value == 'r') || (@value == 'r' && another_hand.value == 's') || (@value == 's' && another_hand.value == 'p')
+      1
+    else
+      -1
+    end
+  end
+end
+      
+
 class Player
-  attr_accessor :choice
+  include Comparable
+
+  attr_accessor :hand
   attr_reader :name
 
   def initialize(n)
@@ -7,7 +30,7 @@ class Player
   end
 
   def to_s
-    "#{name} currently has a choice of #{choice}."
+    "#{name} currently has a choice of #{self.hand.value}."
   end
 end
 
@@ -18,13 +41,13 @@ class Human < Player
       c = gets.chomp.downcase
     end until Game::CHOICES.keys.include?(c)
 
-    self.choice = c
+    self.hand = Hand.new(c)
   end
 end
 
 class Computer < Player
   def pick_hand
-    self.choice = Game::CHOICES.keys.sample
+    self.hand = Hand.new(Game::CHOICES.keys.sample)
   end
 end
 
@@ -38,9 +61,20 @@ class Game
     @computer = Computer.new('R2D2')
   end
 
+  def compare_hands
+    if player.hand == computer.hand
+      puts "TIE"
+    elsif player.hand > computer.hand
+      puts "#{player.name} wins."
+    else
+      puts "#{computer.name} wins."
+    end
+  end
+
   def play
     player.pick_hand
     computer.pick_hand
+    compare_hands
     puts player
     puts computer
   end
